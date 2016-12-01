c7507a3b3564fb4a66c1544d48be5abde83e80eb
Javier Montemayor <j.montemayor.mm@gmail.com>
2016-11-30 23:56:58 -0600

    Fixed the newest files and added comments

--- AuxSistAc.m
+++ AuxSistAc.m
@@ -1,20 +1,25 @@
 function dydt = AuxSistAc(t, y)
 
-% Vector order:
-% y(1) = d\phi
-% y(2) = dv
+%Se inicializa la solucion al sistema de ecuaciones diferenciales
+%acoplados.
 
 n = 0.25;
 M = 2.4;
 
+%Se escriben las constantes.
 F5 = -1*(((8191)/(672))+((583)/(24))*n)*pi;
 F7 = -1*(((16285)/(504))-((214754)/(1728))*n-((193385)/(3024))*n^(2))*pi;
 
-dE = @(v) (v.^2*((816334744563213*v.^5)/140737488355328 + (1069*v.^3)/96 - (13*v)/24))/8 + (v.*((272111581521071*v.^6)/281474976710656 + (1069*v.^4)/384 - (13*v.^2)/48))/4;
-F = @(v) ((32)/(5))*n^(2)*v.^(10).*(1-1*(((1247)/(336))+((35)/(12))*n)*v.^2-4*pi*v.^3+F5*v.^5+(((6643739519)/(69854400))+((16)/(3))*pi^(2)-((1712)/(105))*0.577216+(((41)/(48))*pi^(2)-((134543)/(7776)))*n-((94403)/(3024))*n^(2)-((775)/(324))*n^(3)-((856)/(105))*log(16*v.^(2))).*v.^6+F7*v.^7);
+%Estas ecuaciones son las propuestas por el metodo de expansion, las cuales
+%son usadas para resolver el sistema y asi poder obtener la velocidad en
+%funcion del tiempo, y despues la graficacmos al cuadrado para comparar con
+%el resultado analitico.
+dE = @(v) (v.^2.*((816334744563213*v.^5)/140737488355328 + (1069*v.^3)/96 - (13*v)/24))/8 + (v.*((272111581521071*v.^6)/281474976710656 + (1069*v.^4)/384 - (13*v.^2)/48))/4;
+F = @(v) ((32)/(5))*n^(2)*v.^(10).*(1-1*(((1247)/(336))+((35)/(12))*n)*v.^2-4*pi*v.^3+F5*v.^5+(((6643739519)/(69854400))+((16)/(3))*pi^(2)-((1712)/(105))*0.577216+(((41)/(48))*pi^(2)-...
+((134543)/(7776)))*n-((94403)/(3024))*n^(2)-((775)/(324))*n^(3)-((856)/(105))*log(16*v.^(2))).*v.^6+F7*v.^7);
 
+%Se define el sistema.
+dydt = [(y(1).^3)./M; ... % d\phi/dt
+    -F(y(1))./(M.*dE(y(1)))]; % dv/dt
 
-dydt = [(y(2).^3)./M;... % d\phi/dt
-    -F(y(2))./(M.*dE(y(2)))]; % dv/dt
 end
-
--- SistemaAcoplado.m
+++ SistemaAcoplado.m
@@ -1,7 +1,11 @@
 clear
 
-% Vector order:
-% y(1) = d\phi
-% y(2) = dv
+%Se hace uso de ode15s para invocar la solucion al sistema acoplado.
+[t, y] = ode15s(@AuxSistAc, 0.1:0.01:30, [.198, -10]);
 
-[t, y] = ode45(@AuxSistAc, 0.1:0.001:30, [0; 200]);
+%Las condiciones iniciales influira en la amplitud de la solucion para la
+%velocidad, ya que nos interesa hacer la comparacion con este nuevo metodo y el ya implementado anteriormente.
+figure(1)
+plot(t,y(:,1).^2)
+xlabel('Tiempo')
+ylabel('v^2(t)')
